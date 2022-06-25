67e64e72a0d71c013a086030e83fc57d521754bc
jacky chen <jackychen4@gmail.com>
2022-06-25 09:00:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 import pygame
+
+class Menu(object):
+    def __init__(self, title) -> None: #class for menu 
+        self.title = title
+    def testFunc (self)-> str:
+        return self.title
+
+class gameInstance(Menu): #class for gameInstance
+    def __init__(self) -> None:
+        pass
+class Arena(gameInstance): #class for Arena 
+    def __init__(self) -> None:
+        pass
+
+class Entity(Arena): #class for Entity
+    def __init__(self) -> None:
+        pass
+
+class Tank(Entity): #class for Tank
+    def __init__(self, dmg) -> None:
+        self.dmg = dmg
+
+
+class obstacle(Entity): #class for obstacle
+    def __init__(self, width, height) -> None:
+        self.height = height
+        self.width = width
+        
+class Powerup(Entity): #class for Powerup
+    def __init__(self, powerupName) -> None:
+        self.powerupName = powerupName
